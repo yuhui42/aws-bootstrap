07848eb854ba7c6e03c576a4f5fe12fc350df512
Your Name <you@example.com>
2022-09-13 18:43:01 +0000

    Add ALB Load Balancer

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -43,9 +43,9 @@ aws cloudformation deploy \
     GitHubPersonalAccessToken=$GH_ACCESS_TOKEN \
     CodePipelineBucket=$CODEPIPELINE_BUCKET
 
-    # If the deploy succeeded, show the DNS name of the created instance
+# If the deploy succeeded, show the DNS name of the created instance
 if [ $? -eq 0 ]; then
   aws cloudformation list-exports \
     --profile awsbootstrap \
-    --query "Exports[?starts_with(Name,'InstanceEndpoint')].Value"
+    --query "Exports[?ends_with(Name,'LBEndpoint')].Value" 
 fi
