22afd185236fd4792e56f88f60d7c83becbc19af
Your Name <you@example.com>
2022-09-14 03:53:29 +0000

    Split out staging nested stack

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const { hostname } = require('os');
 const http = require('http');
-const message = `Hello World from ${hostname()}\n`;
-const port = 8080; 
-const server = http.createServer((req, res) => { 
+const STACK_NAME = process.env.STACK_NAME || "Unknown Stack";
+const message = `Hello World from ${hostname()} in ${STACK_NAME}\n`;
+const port = 8080;
+const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end(message);
 });
-  server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname()}:${port}/`);
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname()}:${port}/`);
 });
